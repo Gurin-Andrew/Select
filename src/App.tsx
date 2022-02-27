65158f4dfa181d6655be50dec9e5d55e0ec88628
Andrew Gurin <gurinos68@gmail.com>
2022-02-28 06:07:06 +0700

    добавлен основной функционал

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,17 @@ import { Select } from './components/select/index'
 
 function App() {
 
-  let data = ["Иркутск", "Томск", "Красноярск", "Питер", "Москва", "Новосибирск"]
+  let datas = ["Иркутск", "Томск", "Красноярск", "Питер", "Москва", "Новосибирск"]
+  const data = datas.map(index => {
+    return ({
+      value: index,
+      id: datas.indexOf(index)
+    })
+  })
   return (
     <div className="App">
       <Select data={data} header="Choose town" multiple={false} ></Select>
+      <Select data={data} header="Choose town" multiple={true}></Select>
     </div>
   );
 }
--- src/components/select/index.tsx
+++ src/components/select/index.tsx
@@ -1,27 +1,93 @@
 import React, { MutableRefObject, useRef, useState } from "react";
-import { SingleSelect } from "./Single/index"
-import { MultipleSelect } from "./Multiple/index"
+import "./style.css"
+
+type data = {
+    id: number,
+    value: string,
+    icon?: string
+}[]
 interface iSelect {
-    data: Array<string>,
-    multiple: boolean,
+    data: data,
     header: string
+    multiple?: boolean
 }
 
-export const Select: React.FC<iSelect> = ({ data, multiple, header }) => {
-    if (multiple == false)
-        return (<SingleSelect data={data} header={header}></SingleSelect>)
-    else
-        return (<MultipleSelect data={data} header={header}></MultipleSelect>)
+export const Select: React.FC<iSelect> = ({ data, header, multiple }) => {
+
+    const [active, setActive] = useState(true)
+
+    const inputEl = useRef() as MutableRefObject<HTMLInputElement>;
+    const clearInput = () => {
+        inputEl.current.value = ""
+    }
+    const [selectedOptions, setSelectedOptions] = useState<Array<string>>([])
+    const [searchValue, setSearchValue] = useState('')
+    const toggleOption = (selectedOption: string) => {
+        if (multiple) {
+            if (selectedOptions.some((option) => option === selectedOption)) {
+                setSelectedOptions([...selectedOptions].filter(item => item !== selectedOption))
+            }
+            else {
+
+                setSelectedOptions([...selectedOptions, selectedOption])
+                console.log(selectedOptions)
+            }
+        }
+
+        else {
+            if (selectedOptions.some((option) => option === selectedOption)) {
+                setSelectedOptions([])
+            }
+            else {
+                setActive(!active)
+                setSelectedOptions([selectedOption])
+            }
+        }
+
+    }
+    let filtredData = data.filter(item => String(item.value.toLowerCase()).indexOf(searchValue.toLowerCase()) != -1)
+    return (<div className="container">
+        <h2 >{header} </h2>
+        < div className="select-box">
+            <div className={"options-container " + (active ? "active" : "")}>
 
 
+                {filtredData.map((data, index) => (
+                    <div className={"option " + (selectedOptions.some(item => item === data.value) ? "isSelected" : '')} key={index} onClick={
+                        () => {
+                            const selectedOption = data.value
+                            toggleOption(selectedOption)
+                        }
+                    }
+
+
+                    >
+                        <input type="radio" className="radio" id={data.value} name="category" />
+                        <label htmlFor={data.value}>{data.value}</label>
+
+                    </div>
+                ))}
+
+            </div>
+            <div className="selected" onClick={() => {
+                setActive(!active)
+                clearInput()
+                setSearchValue('')
+
+            }
+
+            }>
+                {selectedOptions.length === 0 ? header : selectedOptions.join(', ')}
+            </div>
+            <div className="search-box">
+                <input type="text" ref={inputEl} onChange={(event) => {
+                    setSearchValue(event.target.value)
+                }} placeholder="Start Typing..." />
+            </div>
+        </div>
+
+
+    </div >
+
+    )
 }
-
-
-
-
-
-
-
-
-
-
